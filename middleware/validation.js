00b6d806676bbd1ce5903bd2adba758034af89cd
JLKohl <121449936+JLKohl@users.noreply.github.com>
2026-02-21 11:48:42 -0600

    fixded switched req, res error

--- middleware/validation.js
+++ middleware/validation.js
@@ -83,12 +83,11 @@ const validateTrips = [
     .withMessage('Notes must be a string')
 ]
 
-const handleValidationErrors = (res, req, next) => {
+const handleValidationErrors = (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array() });
     }
-
     next();
 }
 
